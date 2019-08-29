d18cb45c6a9b7b02cbca5f8f84c8db1df66de58c
Jianghong Ying <YJianghong@gmail.com>
2019-08-29 00:08:56 -0400

    change folder structure

---
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/ExpressCourierApplication.java
@@ -0,0 +1,13 @@
+package com.fcv.expressCourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressCourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressCourierApplication.class, args);
+	}
+
+}
