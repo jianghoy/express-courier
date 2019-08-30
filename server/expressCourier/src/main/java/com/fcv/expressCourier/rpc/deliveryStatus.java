f52c7ea6d062e5898dba9d9a364fe317269d12b2
Jianghong Ying <YJianghong@gmail.com>
2019-08-30 00:40:35 -0400

    jianghong's branch

---
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/rpc/deliveryStatus.java
@@ -0,0 +1,12 @@
+package com.fcv.expressCourier.rpc;
+
+import com.fcv.expressCourier.models.Delivery;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class deliveryStatus {
+
+//    @GetMapping("/status")
+//    public Delivery getDeliveryById()
+}
