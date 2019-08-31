3f2280401a7aa9a27172c22a998942ae5e9e6ae3
Jianghong Ying <YJianghong@gmail.com>
2019-08-31 15:48:35 -0500

    Serve static page

--- tutorial/demos/get distance using Distance Matrix API/index.js
+++ tutorial/demos/get distance using Distance Matrix API/index.js
@@ -2,6 +2,9 @@
 // Here 'express' means express.js server, which is a tiny node.js backend server I 
 // often use.
 const express = require('express')
+// Here 'path' is a module that calculates absolute path. express.js can only serve
+// files with absolute path or specified root path(which doesn't make sense here).
+const path = require('path')
 
 // Now app is the express.js server object.
 const app = express()
@@ -9,3 +12,12 @@ const app = express()
 // Set port it listen to onto 3000.
 // e.g. TomCat default port is 8080.
 const port = 3000
+
+// Serve static distance_input.html on root endpoint
+// __dirname is the directory of this folder(module)
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'/distance_input.html'))
+})
+
+// Start server.
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
