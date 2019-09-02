bca8cd1cb634d4173dfc6b7cc727f59e8f66c72a
jackfeng <mickjack2013@gmail.com>
2019-09-01 22:32:33 -0400

    push code to github for first time

--- server/expressCourier/src/main/java/com/fcv/expressCourier/dao/DeliveryRepository.java
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/dao/DeliveryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
+
     @Override
     <S extends Delivery> S save(S s);
 
