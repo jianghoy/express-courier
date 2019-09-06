341d5de1946aac50adad3453fb32cb84020b464e
Yuki Ou <yukk1ou@hotmail.com>
2019-09-05 21:26:32 -0700

    places api source code - need to load library (api key)

--- client/web/src/Components/InfoCard.js
+++ client/web/src/Components/InfoCard.js
@@ -1,8 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Card } from 'antd';
 import { Input } from 'antd';
 import { Radio, Button } from 'antd';
-
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
 
 class InfoCard extends Component {
     state = {
@@ -14,6 +17,18 @@ class InfoCard extends Component {
             value: e.target.value,
         });
     };
+
+    handleChange = address => {
+        this.setState({ address });
+    };
+
+    handleSelect = address => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', latLng))
+            .catch(error => console.error('Error', error));
+    };
+
     render() {
         const radioStyle = {
             display: 'block',
@@ -25,7 +40,44 @@ class InfoCard extends Component {
                 <br />
                 <br />
                 <div className="autocomplete">
-                    <Input placeholder="Add pick up location" />
+                    <PlacesAutocomplete
+                        value={this.state.address}
+                        onChange={this.handleChange}
+                        onSelect={this.handleSelect}
+                    >
+                        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                            <div>
+                                <input
+                                    {...getInputProps({
+                                        placeholder: 'Search Places ...',
+                                        className: 'location-search-input',
+                                    })}
+                                />
+                                <div className="autocomplete-dropdown-container">
+                                    {loading && <div>Loading...</div>}
+                                    {suggestions.map(suggestion => {
+                                        const className = suggestion.active
+                                            ? 'suggestion-item--active'
+                                            : 'suggestion-item';
+                                        // inline style for demonstration purpose
+                                        const style = suggestion.active
+                                            ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                            : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                        return (
+                                            <div
+                                                {...getSuggestionItemProps(suggestion, {
+                                                    className,
+                                                    style,
+                                                })}
+                                            >
+                                                <span>{suggestion.description}</span>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        )}
+                    </PlacesAutocomplete>
                     <br />
                     <br />
                     <Input placeholder="Add destination" />
