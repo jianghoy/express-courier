bf77edbaa294decfcaaa9692ca1460dbd8c1c36b
Jianghong Ying <YJianghong@gmail.com>
2019-09-07 21:14:37 -0700

    remove unused import and add todo

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
 import MainPage from "./Components/MainPage";
 
+
+// TODO: decide how to serve various pages: via router or components
 function App() {
   return (
     <div className="App">
+        {/* {(showMainPage)?<MainPage/>:<anotherPage/>} */}
         <MainPage/>
     </div>
   );
