fee3347438607f65d0e74b06550184a995da72f1
Jianghong Ying <YJianghong@gmail.com>
2019-09-07 21:26:45 -0700

    rename component into <OrderPanel>; move <div> into <OrderPanel>

--- client/web/src/Components/MainPage.js
+++ client/web/src/Components/MainPage.js
@@ -1,17 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import NavBar from "./NavBar";
 import SimpleMap from "./SimpleMap";
-import InfoCard from "./InfoCard";
+import OrderPanel from "./OrderPanel";
 
+//TODO:determine should we merge MainPage with App
 class MainPage extends Component {
     render() {
         return (
             <div className="MainPage">
-                <NavBar/>
-                <SimpleMap className="Map"/>
-                <div className="infoCard">
-                    <InfoCard/>
-                </div>
+                <NavBar />
+                <SimpleMap className="Map" />
+                {/* //TODO: put className on InfoCard component */}
+                {/* <InfoCard className = "infoCard"/> */}
+                <OrderPanel />
             </div>
         );
     }
