0427ea176ff8fcd0a3c6c0ba80a94a273009ebcb
bigbigfa <bigbigfa@gmail.com>
2019-09-08 23:48:22 -0400

    add two controlers

--- server/expressCourier/src/main/java/com/fcv/expressCourier/controller/DeliveryController.java
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/controller/DeliveryController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-public class OrderController {
+public class DeliveryController {
     @RequestMapping(value = "/order/{orderID}", method = RequestMethod.GET)
-    public Order getOrder(@PathVariable("orderId") int orderId) {
+    public String getDeliveryStatus(@PathVariable("orderId") int orderId) {
         // Order order = orderDao.getOrder(orderId)
         Order order = new Order();
-        return order;
+        return order.getStatus();
     }
 }
