8a00eccdca77f71a5af660543fd1f927a3411d5a
jackfeng <mickjack2013@gmail.com>
2019-09-10 02:20:04 -0400

    partially complete

--- client/web/src/Components/MainPage.js
+++ client/web/src/Components/MainPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NavBar from "./NavBar";
 import SimpleMap from "./SimpleMap";
 import OrderPanel from "./OrderPanel";
+import OrderList from "./OrderList";
 
 //TODO:determine should we merge MainPage with App
 class MainPage extends Component {
@@ -12,6 +13,7 @@ class MainPage extends Component {
                 <SimpleMap className="Map" />
                 {/* //TODO: put className on InfoCard component */}
                 {/* <InfoCard className = "infoCard"/> */}
+                <OrderList />
                 <OrderPanel />
             </div>
         );
---
+++ client/web/src/Components/OrderList.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import { List, message, Avatar, Spin } from 'antd';
+import reqwest from 'reqwest';
+import InfiniteScroll from 'react-infinite-scroller';
+
+// const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
+
+const data = [
+  {
+    title: 'Ant Design Title 1',
+    
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+  {
+    title: 'Ant Design Title 5',
+  },
+  {
+    title: 'Ant Design Title 6',
+  },
+  {
+    title: 'Ant Design Title 6',
+  },
+  {
+    title: 'Ant Design Title 7',
+  },
+];
+
+class OrderList extends Component {
+  state = {
+    loading: false,
+    hasMore: true,
+  };
+
+  componentDidMount() { 
+    this.fetchData(res => {
+      this.setState({
+        data: res.results,
+      });
+    });
+  }
+
+  fetchData = callback => {
+    reqwest({
+      // url: fakeDataUrl,
+      type: 'json',
+      method: 'get',
+      contentType: 'application/json',
+      success: res => {
+        callback(res);
+      },
+    });
+  };
+
+  handleInfiniteOnLoad = () => {
+    let { data } = this.state;
+    this.setState({
+      loading: true,
+    });
+    if (data.length > 6) {
+      message.warning('Infinite List loaded all');
+      this.setState({
+        hasMore: false,
+        loading: false,
+      });
+      return;
+    }
+    this.fetchData(res => {
+      data = data.concat(res.results);
+      this.setState({
+        data,
+        loading: false,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="orderList">
+        <InfiniteScroll
+          initialLoad={false}
+          pageStart={0}
+          loadMore={this.handleInfiniteOnLoad}
+          hasMore={!this.state.loading && this.state.hasMore}
+          useWindow={false}
+        >
+          {/* <List
+            dataSource={this.state.data}
+            renderItem={item => (
+              <List.Item key={item.id}>
+                <List.Item.Meta
+                  avatar={
+                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                  }
+                  title={<a href="https://ant.design">{item.name.last}</a>}
+                  description={item.email}
+                />
+                <div>Content</div>
+              </List.Item>
+            )}
+          >
+          </List> */}
+
+          <List 
+              itemLayout="horizontal"
+              dataSource={data}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title={<a href="https://ant.design">{item.title}</a>}
+                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  />
+                </List.Item>
+              )}
+            />
+        </InfiniteScroll>
+      </div>
+    );
+  }
+}
+
+export default OrderList;
