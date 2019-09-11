bc59a5abfac1eb72fa57252b910746b17d4d7859
Jianghong Ying <YJianghong@gmail.com>
2019-09-11 13:02:37 -0700

    add entity tag

--- server/expressCourier/src/main/java/com/fcv/expressCourier/models/WareHouse.java
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/models/WareHouse.java
@@ -3,6 +3,7 @@ package com.fcv.expressCourier.models;
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 public class WareHouse implements Serializable {
     private static final long serialVersionUID = 3670886414868062603L;
     @Id
