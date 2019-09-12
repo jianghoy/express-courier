cc58ebd99e71a3fc81f966a346e644eec1a33d1f
Jianghong Ying <YJianghong@gmail.com>
2019-09-11 17:00:45 -0700

    add order repository

---
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/dao/OrderRepository.java
@@ -0,0 +1,20 @@
+package com.fcv.expressCourier.dao;
+
+import com.fcv.expressCourier.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    @Override
+    Optional<Order> findById(Integer integer);
+
+    @Override
+    <S extends Order> S save(S s);
+
+    @Override
+    List<Order> findAll();
+
+}
