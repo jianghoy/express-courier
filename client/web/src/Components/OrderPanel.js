b8dd94a737036ce286a4ac2d23a2b082c50ef9bc
Jianghong Ying <YJianghong@gmail.com>
2019-09-14 00:56:50 -0700

    reformat

--- client/web/src/Components/OrderPanel.js
+++ client/web/src/Components/OrderPanel.js
@@ -1,69 +1,61 @@
-import React, { Component } from 'react';
-import {Card, Input, Menu, Radio, Button} from 'antd';
+import React, { Component } from "react";
+import { Card, Input, Menu, Radio, Button } from "antd";
 import PlacesAutocomplete, {
     geocodeByAddress,
-    getLatLng,
-} from 'react-places-autocomplete';
-import { Typography} from 'antd';
+    getLatLng
+} from "react-places-autocomplete";
+import { Typography } from "antd";
 import DeliveryOption from "./DeliveryOption";
 import Icon from "antd/es/icon";
 
-const { Title} = Typography;
-
-
+const { Title } = Typography;
 
 class OrderPanel extends Component {
     state = {
-        value: 1,
+        value: 1
     };
-
 
     handlePickUpAddressChange = pickUpInput => {
         this.setState({ pickUpInput });
     };
 
-
     handlePickUpAddressSelect = async pickUpAddress => {
         // TODO: send pickUpAddress to back end
-        await this.setState({pickUpAddress});
-        this.setState({pickUpInput: pickUpAddress});
+        await this.setState({ pickUpAddress });
+        //this.setState({ pickUpInput: pickUpAddress });
         // console.log(this.state.pickUpAddress);
         geocodeByAddress(pickUpAddress)
             .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
-            .catch(error => console.error('Error', error));
-
+            .then(latLng => console.log("Success", latLng))
+            .catch(error => console.error("Error", error));
     };
     handleDestinationAddressChange = destinationInput => {
         this.setState({ destinationInput });
     };
 
-
     handleDestinationAddressSelect = async destinationAddress => {
         // TODO: send destinationAddress to back end
-        await this.setState({destinationAddress});
-        this.setState({destinationInput: destinationAddress});
+        await this.setState({ destinationAddress });
+        //this.setState({ destinationInput: destinationAddress });
         // console.log(this.state.destinationAddress);
         geocodeByAddress(destinationAddress)
             .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
-            .catch(error => console.error('Error', error));
-
+            .then(latLng => console.log("Success", latLng))
+            .catch(error => console.error("Error", error));
     };
 
-    handlePickUpClear = e =>{
+    handlePickUpClear = e => {
         this.setState({
             pickUpAddress: "",
-            pickUpInput: "",
-        })
+            pickUpInput: ""
+        });
     };
-    handleDestinationClear = e =>{
+    handleDestinationClear = e => {
         this.setState({
             destinationAddress: "",
-            destinationInput: "",
-        })
+            destinationInput: ""
+        });
     };
-
 
     /* Turn PlacesAutocomplete into a component, make code easier to read*/
     render() {
@@ -76,40 +68,65 @@ class OrderPanel extends Component {
                             <PlacesAutocomplete
                                 value={this.state.pickUpInput}
                                 onChange={this.handlePickUpAddressChange}
-                                onSelect={this.handlePickUpAddressSelect}>
-                                { ({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                                onSelect={this.handlePickUpAddressSelect}
+                            >
+                                {({
+                                    getInputProps,
+                                    suggestions,
+                                    getSuggestionItemProps,
+                                    loading
+                                }) => (
                                     <div className="autocomplete-input">
                                         <Input
                                             addonAfter={
-                                                <Icon type="close-circle-o"
-                                                    onClick={ this.handlePickUpClear}
+                                                <Icon
+                                                    type="close-circle-o"
+                                                    onClick={
+                                                        this.handlePickUpClear
+                                                    }
                                                 />
                                             }
                                             {...getInputProps({
-                                                placeholder: "Add Pick Up Location",
-                                                className: 'location-search-input',
-                                                autoFocus: true,
+                                                placeholder:
+                                                    "Add Pick Up Location",
+                                                className:
+                                                    "location-search-input",
+                                                autoFocus: true
                                             })}
                                         />
                                         <Menu className="autocomplete-dropdown-container">
                                             {/*{loading && <div>Loading...</div>}*/}
                                             {suggestions.map(suggestion => {
                                                 const className = suggestion.active
-                                                    ? 'suggestion-item--active'
-                                                    : 'suggestion-item';
+                                                    ? "suggestion-item--active"
+                                                    : "suggestion-item";
                                                 // inline style for demonstration purpose
                                                 const style = suggestion.active
-                                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                                    ? {
+                                                          backgroundColor:
+                                                              "#fafafa",
+                                                          cursor: "pointer"
+                                                      }
+                                                    : {
+                                                          backgroundColor:
+                                                              "#ffffff",
+                                                          cursor: "pointer"
+                                                      };
                                                 return (
                                                     <Menu.Item
-                                                        {...getSuggestionItemProps(suggestion, {
-                                                            className,
-                                                            style,
-                                                        })}>
-                                                       <span>
-                                                        {suggestion.description}
-                                                       </span>
+                                                        {...getSuggestionItemProps(
+                                                            suggestion,
+                                                            {
+                                                                className,
+                                                                style
+                                                            }
+                                                        )}
+                                                    >
+                                                        <span>
+                                                            {
+                                                                suggestion.description
+                                                            }
+                                                        </span>
                                                     </Menu.Item>
                                                 );
                                             })}
@@ -124,40 +141,64 @@ class OrderPanel extends Component {
                                 onChange={this.handleDestinationAddressChange}
                                 onSelect={this.handleDestinationAddressSelect}
                             >
-                                { ({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                                {({
+                                    getInputProps,
+                                    suggestions,
+                                    getSuggestionItemProps,
+                                    loading
+                                }) => (
                                     <div className="autocomplete-input">
                                         <Input
                                             addonAfter={
-                                                <Icon type="close-circle-o"
-                                                      onClick={ this.handleDestinationClear}
+                                                <Icon
+                                                    type="close-circle-o"
+                                                    onClick={
+                                                        this
+                                                            .handleDestinationClear
+                                                    }
                                                 />
                                             }
                                             {...getInputProps({
-                                                placeholder: "Add Your Destination",
-                                                className: 'location-search-input',
-                                                autoFocus: true,
+                                                placeholder:
+                                                    "Add Your Destination",
+                                                className:
+                                                    "location-search-input",
+                                                autoFocus: true
                                             })}
                                         />
                                         <Menu className="autocomplete-dropdown-container">
                                             {/*{loading && <div>Loading...</div>}*/}
                                             {suggestions.map(suggestion => {
                                                 const className = suggestion.active
-                                                    ? 'suggestion-item--active'
-                                                    : 'suggestion-item';
+                                                    ? "suggestion-item--active"
+                                                    : "suggestion-item";
                                                 // inline style for demonstration purpose
                                                 const style = suggestion.active
-                                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                                    ? {
+                                                          backgroundColor:
+                                                              "#fafafa",
+                                                          cursor: "pointer"
+                                                      }
+                                                    : {
+                                                          backgroundColor:
+                                                              "#ffffff",
+                                                          cursor: "pointer"
+                                                      };
                                                 return (
                                                     <Menu.Item
-                                                        {...getSuggestionItemProps(suggestion, {
-                                                            className,
-                                                            style,
-                                                        })}
+                                                        {...getSuggestionItemProps(
+                                                            suggestion,
+                                                            {
+                                                                className,
+                                                                style
+                                                            }
+                                                        )}
                                                     >
-                                                       <span>
-                                                        {suggestion.description}
-                                                       </span>
+                                                        <span>
+                                                            {
+                                                                suggestion.description
+                                                            }
+                                                        </span>
                                                     </Menu.Item>
                                                 );
                                             })}
@@ -168,8 +209,11 @@ class OrderPanel extends Component {
                         </div>
                     </div>
 
-                    <DeliveryOption pickUpAddress={this.state.pickUpAddress} destinationAddress={this.state.destinationAddress}/>
-                    
+                    <DeliveryOption
+                        pickUpAddress={this.state.pickUpAddress}
+                        destinationAddress={this.state.destinationAddress}
+                    />
+
                     <div className="checkout">
                         <Button type="primary" block>
                             Checkout
@@ -177,7 +221,6 @@ class OrderPanel extends Component {
                     </div>
                 </Card>
             </div>
-
         );
     }
 }
