441ce2e12da4153d4a2bff28c7ac9ab507a3f263
Jianghong Ying <YJianghong@gmail.com>
2019-09-28 04:46:08 -0700

    remove comments

--- client/web/src/Components/DeliveryOption.js
+++ client/web/src/Components/DeliveryOption.js
@@ -54,74 +54,7 @@ class DeliveryOption extends Component {
         }
 
         return (
-            <div className="radio_button">
-                {/* <Radio.Group
-                    onChange={this.onRadioButtonChange}
-                    value={this.state.value}
-                >
-                    <Row className="row-selection" gutter={30}>
-                        <Col span={10}>
-                            <Radio
-                                className="radio"
-                                style={radioStyle}
-                                value={"car"}
-                            >
-                                Use Car
-                            </Radio>
-                        </Col>
-                        <Col span={14}>
-                            <Row type="flex" align="middle" gutter={7}>
-                                <Col span={12}>
-                                    <Statistic
-                                        className="price"
-                                        prefix="$"
-                                        value={this.state.carPrice}
-                                        precision={2}
-                                    />
-                                </Col>
-                                <Col span={12}>
-                                    <Statistic
-                                        className="time"
-                                        suffix="AM"
-                                        value="11:24"
-                                        precision={2}
-                                    />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row className="row-selection" gutter={30}>
-                        <Col span={10}>
-                            <Radio
-                                className="radio"
-                                style={radioStyle}
-                                value={"drone"}
-                            >
-                                Use Drone
-                            </Radio>
-                        </Col>
-                        <Col span={14}>
-                            <Row type="flex" align="middle" gutter={7}>
-                                <Col span={12}>
-                                    <Statistic
-                                        className="price"
-                                        prefix="$"
-                                        value={this.state.dronePrice}
-                                        precision={2}
-                                    />
-                                </Col>
-                                <Col span={12}>
-                                    <Statistic
-                                        className="time"
-                                        suffix="PM"
-                                        value="5:23"
-                                        precision={2}
-                                    />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Radio.Group> */}
+            <div className="radio_button">                
                 <Radio.Group
                     onChange={this.onRadioButtonChange}
                     value={this.state.value}
