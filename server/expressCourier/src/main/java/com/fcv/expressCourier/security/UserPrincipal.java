2e4be5fcf06e0402158f965eeb14319b595c259f
Jianghong Ying <YJianghong@gmail.com>
2019-09-27 22:35:48 -0700

    fix bug: UserPrinciple is not returning user details

--- server/expressCourier/src/main/java/com/fcv/expressCourier/security/UserPrincipal.java
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/security/UserPrincipal.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -87,17 +86,19 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
+    }
+
+
+
+    @Override
+    public String getUsername() {
+        return username;
     }
 
     @Override
     public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
+        return password;
     }
 
     @Override
