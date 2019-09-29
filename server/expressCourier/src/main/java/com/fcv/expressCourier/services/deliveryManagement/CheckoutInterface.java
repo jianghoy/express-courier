de3a72ce041ab03e3551ccccf24e457230b039c2
Jianghong Ying <YJianghong@gmail.com>
2019-09-29 13:22:45 -0700

    add save user functionality

--- server/expressCourier/src/main/java/com/fcv/expressCourier/services/deliveryManagement/CheckoutInterface.java
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/services/deliveryManagement/CheckoutInterface.java
@@ -1,9 +1,10 @@
 package com.fcv.expressCourier.services.deliveryManagement;
 
 import com.fcv.expressCourier.models.Order;
+import com.fcv.expressCourier.security.UserPrincipal;
 
 public interface CheckoutInterface {
     boolean isValid(Order order);
-    boolean placeOrder(Order order);
+    boolean placeOrder(Order order, UserPrincipal currentUser);
 
 }
--- server/expressCourier/src/main/java/com/fcv/expressCourier/services/deliveryManagement/CheckoutService.java
+++ server/expressCourier/src/main/java/com/fcv/expressCourier/services/deliveryManagement/CheckoutService.java
@@ -1,18 +1,37 @@
 package com.fcv.expressCourier.services.deliveryManagement;
 
+import com.fcv.expressCourier.dao.CustomerRepository;
 import com.fcv.expressCourier.dao.OrderRepository;
+import com.fcv.expressCourier.dao.UserRepository;
 import com.fcv.expressCourier.models.Address;
+import com.fcv.expressCourier.models.Customer;
 import com.fcv.expressCourier.models.Order;
+import com.fcv.expressCourier.models.User;
+import com.fcv.expressCourier.security.UserPrincipal;
+import com.fcv.expressCourier.services.priceCalculator.PriceCalculator;
+import com.fcv.expressCourier.utils.AddressToString;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CheckoutService implements CheckoutInterface {
     private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
+    private final UserRepository userRepository;
     private final RobotAssignmentInterface robotAssignmentInterface;
+    private final PriceCalculator priceCalculator;
+    private final AddressToString addressToString;
 
-    public CheckoutService(OrderRepository orderRepository, RobotAssignmentInterface robotAssignmentInterface) {
+
+    public CheckoutService(OrderRepository orderRepository, RobotAssignmentInterface robotAssignmentInterface, UserRepository userRepository, CustomerRepository customerRepository, UserRepository userRepository1, PriceCalculator priceCalculator, AddressToString addressToString) {
         this.orderRepository = orderRepository;
         this.robotAssignmentInterface = robotAssignmentInterface;
+        this.customerRepository = customerRepository;
+
+        this.userRepository = userRepository1;
+        this.priceCalculator = priceCalculator;
+        this.addressToString = addressToString;
     }
 
     @Override
@@ -27,9 +46,21 @@ public class CheckoutService implements CheckoutInterface {
     }
 
     @Override
-    public boolean placeOrder(Order order){
+    public boolean placeOrder(Order order, UserPrincipal currentUser){
         try {
             //order.setRobot(robotAssignmentInterface.findRobot(order));
+            Optional<User> u = userRepository.findByNameOrEmail(currentUser.getEmail(),currentUser.getEmail());
+            if (!u.isPresent()) {
+                return false;
+            }
+            Customer c = u.get().getCustomer();
+            order.setCustomer(c);
+            order.setStatus("waiting to be scheduled");
+
+            if (order.getType().equals("car")) {
+                order.setPrice(priceCalculator.carPrice(addressToString.conversion(order.getPickUpAddress()),
+                        addressToString.conversion(order.getShippingAddress())));
+            }
             orderRepository.save(order);
         } catch (Exception e) {
             e.printStackTrace();
